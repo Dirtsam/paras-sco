d20216318572276874460424dc8fe0a17b88ea46
Sanjay Sushiv <84235832+Dirtsam@users.noreply.github.com>
2023-03-23 21:50:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,15 +7,38 @@ let clientEmail = document.getElementById('userEmail').value;
 let body = 'Full Name :' + fullName + '<br> Contact :' + phoneNumber + '<br> email :' + clientEmail;
 
 Email.send({
-SecureToken : "c05ae092-79b6-4d6c-93de-924fbbe673b8",
-To : 'sanjay13wan@gmail.com',
-From :'sanjay13realty@gmail.com',
-Subject : "This is the subject",
+SecureToken : "c2f962d2-a64a-4525-83de-56b8cbf20c6e",
+To : 'propgrowofficial@gmail.com',
+From :'amitmaggu591@gmail.com',
+Subject : "Paras SCO",
 Body : body
 }).then(
 message => alert(message)
 );
 }
+
+
+// contact page mail 
+function contactEmail(){
+  
+  let fullName = document.getElementById('cName').value;
+  let phoneNumber = document.getElementById('cPhone').value;
+  let clientEmail = document.getElementById('cEmail').value;
+  
+  let body = 'Full Name :' + fullName + '<br> Contact :' + phoneNumber + '<br> email :' + clientEmail;
+  
+  Email.send({
+  SecureToken : "c2f962d2-a64a-4525-83de-56b8cbf20c6e",
+  To : 'propgrowofficial@gmail.com',
+  From :'amitmaggu591@gmail.com',
+  Subject : "Paras SCO",
+  Body : body
+  }).then(
+  message => alert(message)
+  );
+  }
+  
+
 
 // phone number form
 function sendNum(){
@@ -26,10 +49,10 @@ function sendNum(){
     
     let body = "Contact Num:"+ phoneNumber ;  
     Email.send({
-    SecureToken : "c05ae092-79b6-4d6c-93de-924fbbe673b8",
-    To : 'sanjay13wan@gmail.com',
-    From :'sanjay13realty@gmail.com',
-    Subject : "This is the subject",
+    SecureToken : "c2f962d2-a64a-4525-83de-56b8cbf20c6e",
+    To : 'propgrowofficial@gmail.com',
+    From :'amitmaggu591@gmail.com',
+    Subject : "Paras SCO",
     Body : body
     }).then(
     message => alert(message)
@@ -63,3 +86,33 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+
+// nav-hamburger{
+
+const navSlide = ()=>{
+  const hBurger = document.querySelector('.burger');
+   const navBar= document.querySelector('.nav-links');
+   const navLinks = document.querySelectorAll('.nav-links li');
+  //  togggle nav
+   
+   hBurger.addEventListener('click', ()=>{
+    navBar.classList.toggle('nav-active');
+   });
+  //  Animation links
+   navLinks.forEach((link, index) =>{ 
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +4}s`;
+
+   });
+}
+
+navSlide(); 
